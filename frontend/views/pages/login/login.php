5ea7dea96914832eaafa099735b516e45985d7ac
Yorfad <alexmej267@gmail.com>
2025-03-08 15:20:42 -0600

    create; create the log in container en logi.php and implement function changeForm, this is created later

---
+++ frontend/views/pages/login/login.php
@@ -0,0 +1,14 @@
+
+        <!-- Formulario de inicio de sesión -->
+<section class="section section__form--log-in visible">
+    <form action="" class="log-in">
+        <h1 class="tittle">Log in</h1>
+        <input class="input" placeholder="username" type="text" class="username" id="username-log-in">
+        <input class="input" placeholder="password" type="password" class="password" id="password-log-in">
+        <a href="#" class="link">Forgot your password?</a>
+        <button class="btn btn__form" id="boton-log-in">Get into</button>
+        <button class="btn btn__form--change" type="button" onclick="changeForm('signup')">
+            Register
+        </button>
+    </form>
+</section>

e90107b175ddf3d22136d40e624c9ee7fc6a76cf
Yorfad <alexmej267@gmail.com>
2025-03-08 15:14:42 -0600

    fix: fix the routes of portforlio/modules/items to up the images

--- frontend/views/pages/portfolio/modules/items/portfolio_item_four.php
+++ frontend/views/pages/portfolio/modules/items/portfolio_item_four.php
@@ -4,6 +4,6 @@
     <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
       <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
     </div>
-    <img class="img-fluid" src="<?php echo $path ?>/views/assets/img/portfolio/game.png" alt="..." />
+    <img class="img-fluid" src="/frontend/views/assets/img/portfolio/game.png" alt="..." />
   </div>
 </div>

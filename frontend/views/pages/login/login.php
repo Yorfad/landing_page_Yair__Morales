045044f43d83e298f348eb0b9b9d4dc3a483ef90
Yorfad <alexmej267@gmail.com>
2025-03-08 15:25:44 -0600

    create: the sing-up form to create new users

--- frontend/views/pages/login/login.php
+++ frontend/views/pages/login/login.php
@@ -12,3 +12,26 @@
         </button>
     </form>
 </section>
+
+
+<section class="section section__formulario--sign-up">
+    <form action="" class="sign-up">
+        <h1 class="tittles">Register</h1>
+        <input class="input" placeholder="Ingrese su nombre" type="text" class="nombre" id="nombre-sign-up" required>
+        <input class="input" placeholder="Ingrese su apellido" type="text" class="apellido" id="apellido-sign-up" required>
+        <input class="input" placeholder="Ingrese su correo electrónico" type="email" class="correo" id="correo-sign-up" required>
+        <input class="input" placeholder="Contraseña" type="password" name="contraseña" class="contraseña" id="contraseña-sign-up" required>
+        <label for="contraseña" class="texto" id="labelContraseñaSignIn">
+        The password must contain at least 1 uppercase, number, special characters (@#$%^&¿?).
+        </label>
+        <input class="input" placeholder="Confirme su contraseña" type="password" name="confirmarContraseña" class="contraseña" id="confirmarContraseña-sign-up" required>
+        <label for="confirmarContraseña" class="texto" id="labelConfirmarContraseñaSignIn">
+        Passwords must match
+        </label>
+        <a href="#" class="link">Forgot your password?</a>
+        <button class="btn btn__form" id="boton-sign-up">Register</button>
+        <button class="btn btn__form--change" type="button" onclick="changeForm('login')">
+            Log in
+        </button>
+    </form>
+</section>
